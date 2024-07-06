57773bdeb7fe7f5f64ab6c7530b03ed6e5d2271e
samirwadkar31 <74359548+samirwadkar31@users.noreply.github.com>
2024-07-06 23:49:51 +0530

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add few above!</p>
+                <p className="text-center">No items yet! Add fewww above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
